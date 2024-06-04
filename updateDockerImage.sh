1ea8ee7fc1084baf7349ee3292b98bda5ee8cf98
Chris Brunner <cjbrunner@gmail.com>
2024-06-04 22:24:10 +0000

    Add script to update, stop, and start docker image

---
+++ updateDockerImage.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Building new container..."
+docker build . -t nextjs-docker
+echo "Stopping container..."
+docker stop `docker ps | grep nextjs-docker | awk '{print $1}'`
+echo "Starting new container..."
+docker run -p 3001:3000 -d --restart unless-stopped nextjs-docker
