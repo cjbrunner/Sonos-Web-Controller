d5001eb9bf86e9408551171c326f9e7b03817740
Chris Brunner <cjbrunner@github.com>
2024-06-12 13:47:43 -0700

    Testing out basic db connectivity

--- src/app/chrisweb/page.jsx
+++ src/app/chrisweb/page.jsx
@@ -1,5 +1,22 @@
-export default function Page() {
+import pool from '../lib/db';
+
+const getData = async () => {
+  try {
+    const {rows} = await pool.query('SELECT * FROM test');
+    console.log(`result: ${JSON.stringify(rows)}`)
+    return rows
+  } catch (error) {
+    console.log(`error: ${JSON.stringify(error)}`)
+  }
+}
+
+export default async function Page() {
+  const value = await getData();
+  console.log(`value: ${JSON.stringify(value)}`)
   return (
-    <div>Future home of the ChrisWeb archive</div>
+    <div>
+      <h1>Future home of the ChrisWeb archive</h1>
+      <h3>Values: {value.map(r => r.title)}</h3>
+    </div>
   )
 }
